12b99308e411783d31ffeef4c53649024a34a138
Christopher Elwell <elwell.christopher@gmail.com>
2019-01-20 02:10:59 -0800

    select serving size for quantity transformation

--- src/components/Recipes/RecipeDetail.js
+++ src/components/Recipes/RecipeDetail.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import Ingredients from './Ingredients';
+import math from 'mathjs';
 
 class RecipesDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      targetServings: props.recipe.yield,
+    };
+  }
+  
   parseIngredientLines(ingredientLines) {
     return ingredientLines.map(
       line => {
@@ -12,22 +20,53 @@ class RecipesDetail extends Component {
     );
   }
 
+  decimalToFraction(value) {
+    if (value < 1) {
+      return math.format(math.fraction(value), { fraction: 'ratio' });
+    } else {
+      return value;
+    }
+  }
+  
   transformServings(ingredients, ratio) {
     return ingredients.map(
       (ingredient) => {
-        return {...ingredient, quantity: (ingredient.quantity * ratio)};
+        const quantity = parseInt(ingredient.quantity);
+        if (isNaN(quantity)) {
+          return ingredient;
+        } else {
+          return {...ingredient,
+                  quantity: this.decimalToFraction(quantity * ratio)};
+        }
       }
     );
   }
+
+  onChangeTargetServings = (e) => {
+    this.setState({
+      targetServings: e.target.value
+    });
+  }
   
   render() {
-    const { label, image, ingredientLines, yield: servings } = this.props.recipe;
+    const { recipe } = this.props;
+    const { targetServings } =  this.state;
+    const { label, image, ingredientLines, yield: servings } = recipe;
     // todo make selector
-    const servingsRatio = 2 / servings;
+    const servingsRatio = targetServings / servings;
 
     return (
       <div className="RecipesDetail">
         <h2>{label}</h2>
+        <label>
+          Number of Servings:&nbsp;
+          <select value={targetServings}
+                  onChange={this.onChangeTargetServings}>
+            {Array(20).fill().map((v, i) => (
+              <option value={i + 1}>{i + 1}</option>
+            ))}
+          </select>
+        </label>
         <Ingredients
           ingredients={this.transformServings(
             this.parseIngredientLines(ingredientLines),
